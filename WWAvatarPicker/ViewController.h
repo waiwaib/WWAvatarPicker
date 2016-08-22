28a244a15f45c3c06a4966debebf390fbe407dc5
waiwaib <857557118@qq.com>
2016-08-22 09:46:03 +0800

    Initial Commit

---
+++ WWAvatarPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWAvatarPicker
+//
+//  Created by hewaiwai on 16/8/22.
+//  Copyright © 2016年 waiwai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWAvatarPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WWAvatarPicker
+//
+//  Created by hewaiwai on 16/8/22.
+//  Copyright © 2016年 waiwai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
